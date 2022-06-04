022704c5616bcee10d233732010f1facd154aa01
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-04 23:25:09 +0300

    App.js changed

--- RentACar.Client/rent-a-car-client/src/App.js
+++ RentACar.Client/rent-a-car-client/src/App.js
@@ -2,6 +2,16 @@ import "./App.scss";
 import { Layout } from "./components/layout/Layout";
 
 function App() {
+  // const [isAuthenticated, setAuthenticated] = useState(
+  //   getToken() ? true : false
+  // );
+
+  // if (getToken()) {
+  //   console.log(getToken());
+  //   setAuthenticated(true);
+  //   console.log(isAuthenticated);
+  // }
+
   return <Layout />;
 }
 
