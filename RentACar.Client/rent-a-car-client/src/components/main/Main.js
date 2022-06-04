c26d7336d3950be157e4b4ebf5593cd949f494d4
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-04 23:26:23 +0300

    Guarder routes configured

--- RentACar.Client/rent-a-car-client/src/components/main/Main.js
+++ RentACar.Client/rent-a-car-client/src/components/main/Main.js
@@ -1,12 +1,99 @@
-import { Route, Routes } from "react-router-dom";
-import { Register } from "../auth/register/Register";
+import { useRoutes } from "react-router-dom";
+import NonAuthGuard from "./../../services/non-auth-guard-service";
+import { Register } from "./../auth/register/Register";
 import { Login } from "./../auth/login/Login";
+import AuthGuard from "./../../services/auth-guard-service";
+import { UsersList } from "./../users/user-table/UsersList";
+import { EditUser } from "./../users/user-edit-form/EditUser";
+import { CarList } from "../cars/car-list/CarList";
+import { CarForm } from "./../cars/car-form/CarForm";
+import { RentACarForm } from "./../car-rental/rent-a-car-form/RentACarForm";
+import { CarRentalDetails } from "../car-rental/user-car-rentals/CarRentalsDetails";
 
 export function Main() {
-  return (
-    <Routes>
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-    </Routes>
-  );
+  const CustomRoutes = () =>
+    useRoutes([
+      {
+        path: "/login",
+        element: (
+          <NonAuthGuard>
+            <Login />
+          </NonAuthGuard>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <NonAuthGuard>
+            <Register />
+          </NonAuthGuard>
+        ),
+      },
+      {
+        path: "/",
+        element: (
+          <AuthGuard>
+            <UsersList />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/users",
+        element: (
+          <AuthGuard>
+            <UsersList />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/user/edit/:id",
+        element: (
+          <AuthGuard>
+            <EditUser />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/cars",
+        element: (
+          <AuthGuard>
+            <CarList />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/car/edit/:id",
+        element: (
+          <AuthGuard>
+            <CarForm />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/car/add",
+        element: (
+          <AuthGuard>
+            <CarForm />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/car/rent/:id",
+        element: (
+          <AuthGuard>
+            <RentACarForm />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/car-rentals/:id",
+        element: (
+          <AuthGuard>
+            <CarRentalDetails />
+          </AuthGuard>
+        ),
+      },
+    ]);
+
+  return <CustomRoutes />;
 }
