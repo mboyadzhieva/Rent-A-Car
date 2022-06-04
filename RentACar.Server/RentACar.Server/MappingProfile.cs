088d011d174abe7ca94eec36214c76d9d049a813
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-04 23:28:17 +0300

    Mapping added

--- RentACar.Server/RentACar.Server/MappingProfile.cs
+++ RentACar.Server/RentACar.Server/MappingProfile.cs
@@ -5,19 +5,26 @@
     using Features.Cars;
     using Features.Users;
     using Models.Identity;
+    using RentACar.Server.Features.CaRental;
 
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            CreateMap<Car, CarRequestModel>();
-            CreateMap<CarRequestModel, Car>();
+            CreateMap<Car, CarModel>();
+            CreateMap<CarModel, Car>();
 
-            CreateMap<User, UserResponseModel>();
-            CreateMap<UserResponseModel, User>();
+            CreateMap<User, UserModel>();
+            CreateMap<UserModel, User>();
 
             CreateMap<User, RegisterRequestModel>();
             CreateMap<RegisterRequestModel, User>();
+
+            CreateMap<CarRental, CreateCarRentalRequestModel>();
+            CreateMap<CreateCarRentalRequestModel, CarRental>();
+
+            CreateMap<CarRental, CarRentalDetailsResponseModel>();
+            CreateMap<CarRentalDetailsResponseModel, CarRental>();
         }
     }
 }
